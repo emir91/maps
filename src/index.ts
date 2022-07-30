dcb4578d44ee5bd864466ad3c7dfb97543459e03
Emir Vranac <vranacemir@gmail.com>
2022-07-30 14:08:10 +0200

    add google maps api and generate map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { User } from "./User";
-import { Company } from "./Company";
+/// <reference types="@types/google.maps" />
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
+// const user = new User();
 
-console.log(user);
+// console.log(user);
 
-const company = new Company();
+// const company = new Company();
 
-console.log(company);
+// console.log(company);
+
+const mapRef = document.getElementById("map");
+const mapEl: HTMLElement = mapRef as HTMLElement;
+
+new google.maps.Map(mapEl, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
