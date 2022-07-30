ef567c486df8ce0cafe9406b5ba6d0d5f9404ec0
Emir Vranac <vranacemir@gmail.com>
2022-07-30 19:15:06 +0200

    add addMarker functionality to the CustomMap

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,14 +1,31 @@
 /// <reference types="@types/google.maps" />
 
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
-  constructor(mapId: HTMLElement) {
-    this.googleMap = new google.maps.Map(mapId, {
+  constructor(el: HTMLElement) {
+    this.googleMap = new google.maps.Map(el, {
       zoom: 1,
       center: {
         lat: 0,
         lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
 import { CustomMap } from "./CustomMap";
-// import { User } from "./User";
-// import { Company } from "./Company";
+import { User } from "./User";
+import { Company } from "./Company";
 
-// const user = new User();
-
-// console.log(user);
-
-// const company = new Company();
-
-// console.log(company);
+const user = new User();
+const company = new Company();
 
 const mapRef = document.getElementById("map");
 const mapEl: HTMLElement = mapRef as HTMLElement;
 
 const map = new CustomMap(mapEl);
+map.addMarker(user);
+map.addMarker(company);
